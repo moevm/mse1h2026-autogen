41dcb48b73de68fdb0a39ee376435fe41958178f
annaeponil <annaeponil@gmail.com>
2026-03-11 15:34:52 +0300

    Updated task3

--- src/task3/task3.py
+++ src/task3/task3.py
@@ -11,46 +11,100 @@ class QuestionN3(QuestionBase):
 
         random.seed(self.seed)
         self.elementType = random.choice(['odd', 'even'])
-        self.operationType = random.choice(['sum', 'product', 'mask_sum'])
+        self.operationType = random.choice(['sum', 'product', 'bit_and', 'bit_or', 'bit_xor', 'bit_not', 'shift_left', 'shift_right'])
         self.maxLength = 15 if self.operationType == 'product' else 100
+        self.intTypes = ['int', 'long', 'short']
+        self.floatTypes = ['float', 'double']
 
-        self.mask = None
-        if self.operationType == 'mask_sum':
-            self.mask = random.randint(1, 15)
+        if self.operationType in ['sum', 'product']:
+            self.dataType = random.choice(self.intTypes + self.floatTypes)
+        else:
+            self.dataType = random.choice(self.intTypes)
+
+        if self.operationType in ['shift_left', 'shift_right']:
+            self.shiftValue = 1
 
     def generateTest(self) -> tuple[str, str]:
         length = random.randint(10, self.maxLength)
-        borders = {
-            'sum': (0, 100_000_000),
-            'product': (-100, 100), 
-            'mask_sum': (0, 100_000_000)
-        }[self.operationType]
+        match self.dataType:
+            case 'int':
+                borders = (-100_000_000, 100_000_000)
+            case 'short':
+                borders = (-32_768, 32_767)
+            case 'long':
+                borders = (-1_000_000_000, 1_000_000_000)
+            case 'float' | 'double':
+                borders = (-1_000_000, 1_000_000)
+        
+        if self.operationType in ['shift_left','shift_right']:
+            borders = (0, borders[1])
 
-        numbers = [random.randint(*borders) for _ in range(length)]
+        if self.operationType == 'product':
+            borders = (-100, 100)
+
+        if self.dataType in ['int', 'short', 'long']:
+            numbers = [random.randint(*borders) for _ in range(length)]
+        else:
+            numbers = [round(random.uniform(*borders), 3) for _ in range(length)]
+        
         targetNumbers = numbers[(1 if self.elementType == 'odd' else 0)::2]
 
-        if self.operationType == 'mask_sum':
-            programInput = ' '.join([str(x) for x in [length, self.mask] + numbers])
-        else:
-            programInput = ' '.join([str(x) for x in [length] + numbers])
+        programInput = ' '.join([str(x) for x in [length] + numbers])
 
         match self.operationType:
             case 'sum':
                 expectedOutput = sum(targetNumbers)
             case 'product':
                 expectedOutput = reduce(lambda x,y: x*y, targetNumbers, 1)
-            case 'mask_sum':
-                expectedOutput = sum(x for x in targetNumbers if (x & self.mask) == self.mask)
+            case 'bit_and':
+                if not targetNumbers:
+                    expectedOutput = 0
+                else:
+                    expectedOutput = targetNumbers[0]
+                    for num in targetNumbers[1:]:
+                        expectedOutput &= num
+            case 'bit_or':
+                if not targetNumbers:
+                    expectedOutput = 0
+                else:
+                    expectedOutput = targetNumbers[0]
+                    for num in targetNumbers[1:]:
+                        expectedOutput |= num
+            case 'bit_xor':
+                if not targetNumbers:
+                    expectedOutput = 0
+                else:
+                    expectedOutput = targetNumbers[0]
+                    for num in targetNumbers[1:]:
+                        expectedOutput ^= num
+            case 'shift_left':
+                shifted = [x << self.shiftValue for x in targetNumbers]
+                expectedOutput = sum(shifted)
+            case 'shift_right':
+                shifted = [x >> self.shiftValue for x in targetNumbers]
+                expectedOutput = sum(shifted)
+            case 'bit_not':
+                noted = [~x for x in targetNumbers]
+                expectedOutput = sum(noted)
+        
+        if self.dataType in self.floatTypes:
+            expectedOutput = f'{expectedOutput:.6f}'
+        else:
+            expectedOutput = str(expectedOutput)
 
-
-        return programInput, str(expectedOutput)
+        return programInput, expectedOutput
 
     @property
     def questionText(self) -> str:
         operation = {
             'sum': 'сумму',
             'product': 'произведение',
-            'mask_sum': 'сумму элементов массива, у которых установлены все биты, заданные маской M'
+            'bit_and': 'результат побитового И (AND)',
+            'bit_or': 'результат побитового ИЛИ (OR)',
+            'bit_xor': 'результат побитового исключащего ИЛИ (XOR)',
+            'bit_not': 'сумму результатов побитового НЕТ (NOT)',
+            'shift_left': 'сумму результатов сдвига влево на 1',
+            'shift_right': 'сумму результатов сдвига вправо на 1',
         }[self.operationType]
 
         element = {
@@ -72,14 +126,30 @@ class QuestionN3(QuestionBase):
             </table>
             '''
         
-        extra = ''
-        if self.operationType == 'mask_sum':
-            extra = '<br><b>M</b> - битовая маска.'
+        special_notes = []
+        if self.operationType == 'bit_and':
+            special_notes.append(f'''<li>Для пустого массива операция AND возвращает 0</li>''')
+        if self.operationType in ['shift_left', 'shift_right']:
+            special_notes.append(f'''<li>Сдвиг выполняется на константу {self.shiftValue}</li>''')
+            special_notes.append(f'''<li>После сдвига каждого элемента результаты суммируются</li>''')
+        if self.operationType == 'bit_not':
+            special_notes.append(f'''<li>После применения NOT к каждому элементу результаты суммируются</li>''')
+
+        special_cases = ''
+        if special_notes:
+            special_cases = f'''
+            <br>
+            Особые случаи:
+                <ul>
+                    {''.join(special_notes)}
+                </ul>
+            '''
 
         return f'''
-            Напишите программу, на вход которой подаётся сначало число <b>N</b> (<={self.maxLength}), а после - {'<b>M</b> и ' if self.operationType == 'mask_sum' else ''}<b>N</b> целых чисел.
-            {extra}
+            Напишите программу, на вход которой подаётся сначала число <b>N</b> (<={self.maxLength}), а после - <b>N</b> чисел типа <code>{self.dataType}</code>.
             Требуется сохранить числа в статический массив, найти и вывести на экран {operation} элементов массива с {element} индексом.<br><br>
+            {special_cases}
+            <br>
             Пример:
             {exampleTable}
         '''
