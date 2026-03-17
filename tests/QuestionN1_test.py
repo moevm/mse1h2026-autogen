3e8e34db8b99c7c45563f89035671c55d9dd1d72
Denualzzz <maksh4kov@gmail.com>
2026-03-17 11:19:12 +0300

    Revert "Merge pull request #153 from moevm/146-task1-pow-operation"
    This reverts commit cc90784dd854dc871806095d44705472fcc1c3af, reversing
    changes made to 4df371c336f7cc48401ee30d9c75103661b1b4fa.

--- tests/QuestionN1_test.py
+++ tests/QuestionN1_test.py
@@ -3,24 +3,17 @@ from utility import moodleInit
 import pytest
 
 class TestQuestionN1:
-    def setup_method(self):
-        self.question = moodleInit(QuestionN1, seed=123, inputSize=2)
+    question = moodleInit(QuestionN1, seed=123, inputSize=2)
 
     def test_code_preload(self):
         utility.CProgramRunner(self.question.preloadedCode)
 
     def test_question_text(self):
-        self.question.inputBase = 8
-        assert 'числа в <b>восьмеричной</b>' in self.question.questionText
+        assert 'целых чисел в <b>восьмиричной</b>' in self.question.questionText
+        assert 'сумму в <b>шестнадцатиричной</b>' in self.question.questionText
+        assert 'разделены <b>пробелом</b>' in self.question.questionText
 
     def test_code_success_run(self):
-        self.question.inputSize = 2
-        self.question.inputBase = 8 
-        self.question.outputBase = 16
-        self.question.inputFormat = 'spaces'
-        self.question.outputFormat = 'plain'
-        self.question.operation = 'sum'
-
         assert self.question.test(r'''
             #include <stdio.h>
 
@@ -33,44 +26,6 @@ class TestQuestionN1:
                 }
 
                 printf("%x\n", a+b);
-                return 0;
-            }
-        ''') == Result.Ok()
-        
-    def test_code_success_run_pow(self):
-        self.question.inputSize = 3
-        self.question.inputBase = 10 
-        self.question.outputBase = 10
-        self.question.inputFormat = 'spaces'
-        self.question.outputFormat = 'plain'
-        self.question.operation = 'pow'
-
-        assert self.question.test(r'''
-            #include <stdio.h>
-
-            int main() {
-                int n = 3;
-                long long res;
-                                  
-                for (int i = 0; i < n; i++) {
-                    long long val;
-                    if (scanf("%lld", &val) != 1) {
-                        printf("error: %d", i);
-                        return 0;              
-                    }
-                                  
-                    if (i == 0) {
-                        res = val;              
-                    } else {
-                        long long base = res; 
-                        res = 1; 
-                        for (int j = 0; j < val; j++) {
-                            res *= base;          
-                        }              
-                    }
-                }
-               
-                printf("%lld\n", res);
                 return 0;
             }
         ''') == Result.Ok()
