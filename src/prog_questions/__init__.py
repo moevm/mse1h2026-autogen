fc4c0d27e9608c51385069164fd91fe6f8dcb2b1
Настя Мильхерт <n.milkhert@gmail.com>
2026-03-17 16:20:50 +0300

    fix: решение по задаче 151

--- src/prog_questions/__init__.py
+++ src/prog_questions/__init__.py
@@ -5,6 +5,7 @@ from .QuestionStringOperations import QuestionStringOperations
 from .QuestionN1 import QuestionN1
 from .QuestionN2 import QuestionN2
 from .QuestionN3 import QuestionN3
+from .QuestionN4 import QuestionN4
 from .QuestionN5 import QuestionN5
 from .CustomQuestions import *
 from . import utility
