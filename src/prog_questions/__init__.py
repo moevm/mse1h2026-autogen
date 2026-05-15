ff3dcf7cc3d0fde32f27014f05d2f4a6e6b4a3fb
Настя Мильхерт <n.milkhert@gmail.com>
2026-05-15 22:25:00 +0300

    delete not used tasks

--- src/prog_questions/__init__.py
+++ src/prog_questions/__init__.py
@@ -1,7 +1,4 @@
 from .QuestionBase import QuestionBase, Result
-from .QuestionRandomExpression import QuestionRandomExpression
-from .QuestionRandomCondition import QuestionRandomCondition
-from .QuestionStringOperations import QuestionStringOperations
 from .QuestionN1 import QuestionN1
 from .QuestionN2 import QuestionN2
 from .QuestionN3 import QuestionN3
