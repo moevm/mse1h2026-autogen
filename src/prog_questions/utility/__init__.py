b92dbbcbacdd6e6111fac7263a7fa2a087c445c8
annaeponil <annaeponil@gmail.com>
2026-05-16 16:33:50 +0300

    create CppProgramRunner

--- src/prog_questions/utility/__init__.py
+++ src/prog_questions/utility/__init__.py
@@ -1,2 +1,3 @@
 from .CProgramRunner import CProgramRunner, ExecutionError, CompilationError, ExitCodeHandler, EnvironmentError, InternalError
 from .CommentMetric import CommentMetric
+from .CppProgramRunner import CppProgramRunner
